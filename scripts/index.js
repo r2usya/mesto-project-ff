e93774c661f7dfe956be1b8c12a65e1155dbc41f
Ruslan Izmailov <smail136@yandex.ru>
2024-01-22 17:36:32 +0300

    changes1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,28 @@
+const placesList= document.querySelector('.places__list');
+
+function addCard(element, deleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.cloneNode(true);
+  const title = cardElement.querySelector('.card__title');
+  const img = cardElement.querySelector('.card__image');
+
+    title.textContent = element.name;
+    img.src = element.link;
+    img.alt = element.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard);
+    return cardElement;
+}
+
+function deleteCard(event) {
+  const card = event.target.closest('.card');
+  card.remove();
+}
+
+initialCards.forEach((element) => {
+  placesList.append(addCard(element, deleteCard));
+});
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
@@ -7,3 +32,4 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
